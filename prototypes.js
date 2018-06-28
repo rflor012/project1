7246b08e85cc89b13090ef61afa512f5421cdfba
Rodrigo Flores <rflor012@gmail.com>
2018-06-28 08:59:37 -0400

    complications

--- prototypes.js
+++ prototypes.js
@@ -1,6 +1,6 @@
 //the Map Generator Constructor function
 var map =[
-[0,1,1,0,0,0,0,0,0,0],  // 0 is a movaable tile
+[0,1,1,0,0,0,0,0,0,0],  // 0 is a moveable tile
 [1,1,1,0,0,0,0,0,0,0],  // 1 is a friendly unit
 [0,0,3,0,0,0,3,0,0,0],  // 2 is an enemy
 [0,0,0,3,0,0,0,0,3,0],  // 3 is an obstacle
@@ -13,16 +13,29 @@ var map =[
 ];
 var canMove;
 var map2 = map;
+var r = 0;
+var c = 0;
+
+function Cell(location, row, col, tile){
+  this.location = location;
+  this.row = row;
+  this.col = col;
+  this.tile = tile;
+  this.contents = "empty";
+  this.hero = false;
+  this.enemy = [];
+}
 
 Array.prototype.generateMap = function(){
-  for (var r = 0; r < map.length; ++r) {
-        for (var c = 0; c < map[r].length; ++c) {
+  for (var r = 0; r < map.length; r++) {
+        for (var c = 0; c < map[r].length; c++) {
           switch(map[r][c]) {
             case 0:
             {console.log("This tile is open for movement [" + map.indexOf(map[r]) + " , " + map.indexOf(map[c]) + "]");
-            if(map[r][c] === 0){
-              map2[r][c] = 0;
-              map2[r][c + 1] = 1;
+            if(map[r][c] === 3){
+              cell.appendChild(treeTileMaker());
+              // map2[r][c] = 0;
+              // map2[r][c + 1] = 1;
               console.log("new Position on Map: [" + map2.indexOf(map2[r]) + ", " + map2.indexOf(map2[c + 1]) + "]");
             }
 
@@ -39,12 +52,17 @@ Array.prototype.generateMap = function(){
             console.log("the generateMap function found an obstacle at [" + map.indexOf(map[r]) + " , " + map.indexOf(map[c]) + "]");
 
             break;}
+
+            map2.push(location);
           }
+
         }
-      };
+  };
 
 Map.prototype = Object.create(Array.prototype);
 Map.prototype.constructor = Map;
+
+
 
 
 function Adam(name, healthPoints, damagePoints, idTile, direction) {
@@ -93,7 +111,7 @@ function Adam(name, healthPoints, damagePoints, idTile, direction) {
 
 Adam.prototype = Object.create(Adam.prototype);
 Adam.prototype.constructor = Adam;
-var Adam1 = new Adam("Adam", 100, 20, [x, y], d);
+var Adam1 = new Adam("Adam", 100, 20, [0, 0], "s");
 
 //mage spell user
 function Mage(name, health, damage, mana, magicDamage, idTile, direction) {
@@ -110,7 +128,7 @@ function Mage(name, health, damage, mana, magicDamage, idTile, direction) {
 
 Mage.prototype = Object.create(Adam.prototype);
 Mage.prototype.constructor = Mage;
-var Sorc = new Mage("Mage", 100, 10, 100, 40, [x,y], d);
+var Sorc = new Mage("Mage", 100, 10, 100, 40, [0,1], "s");
 
 //Ranged Attack Character
 function Eve(name, health, damage, rangedAttackDamage, idTile, direction) {
@@ -126,237 +144,132 @@ function Eve(name, health, damage, rangedAttackDamage, idTile, direction) {
 
 Eve.prototype = Object.create(Adam.prototype);
 Eve.prototype.constructor = Eve;
-var Archer = new Eve("Archer",100,20,45,[x, y], d);
+var Archer = new Eve("Archer",100,20,45,[0, 2], "s");
 
 
 //Gad help me
 //integrating mars rover module
-alert("COMMAND LIST: (case sensitive)\n l:left \n r:right \n f:forward \n b:back");
-  var d =["n"];                       //direction
-  var x = 0;  //Math.random                        //X axis (West-East)
-  var y = 0;  //Math.random                        //Y axis (South-North)
-  var t = 0;                          //ditch indicator
-  var inv = 0;                        //invalid count
-var i = prompt("INPUT COMMANDS: ");  //commands
-var compass = {n:"North",s:"South",w:"West",e:"East"};
+// alert("COMMAND LIST: (case sensitive)\n l:left \n r:right \n f:forward \n b:back");
+//   var d =["n"];                       //direction
+//   var x = 0;  //Math.random                        //X axis (West-East)
+//   var y = 0;  //Math.random                        //Y axis (South-North)
+//   var t = 0;                          //ditch indicator
+//   var inv = 0;                        //invalid count
+// var i = prompt("INPUT COMMANDS: ");  //commands
+// var compass = {n:"North",s:"South",w:"West",e:"East"};
 
 // I've decided to take out alerts inside the loop, it messes up the travel log.
 
-console.log("\nADAM'S TRAVEL LOG: ");
-for(a=0; a < i.length; a++) {
-  switch(i[a]){
-    case "f":
-        { if (d[d.length-1] === "n" || d[d.length-1] === "s")
-          { y = moveFY(d,y);
-            d.push(d[d.length-1]);
-            }
-          else if (d[d.length-1] === "w" || d[d.length-1] === "e")
-          { x = moveFX(d,x);
-            d.push(d[d.length-1]);
-            }
-        }
-        break;
-    case "b":
-        { if (d[d.length-1] === "n" || d[d.length-1] === "s")
-          { y = moveBY(d,y);
-            d.push(d[d.length-1]);
-            }
-          else if (d[d.length-1] === "w" || d[d.length-1] === "e")
-          { x = moveBX(d,x);
-            d.push(d[d.length-1]);
-            }
-        }
-        break;
-    case "r":
-        turnRight(d,i,a);
-        break;
-    case "l":
-        turnLeft(d,i,a);
-        break;
-    default:
-        inv += 1;
-        d.push(d[d.length-1]);
-        // alert("invalid command: " + i[a]);
-        break;
-  }
-// OUTPUT RESULTS
-    if (x < 0 || y < 0 || x > 10 || y > 10) {t=1; /*alert("YOU DROVE IT IN A DITCH!")*/}
-    console.log("X:",x + " Y:",y);
-}
-
-console.log("\nADAM IS CURRENTLY FACING:", compass[d[d.length-1]]);
-if (t === 1){
-console.log("\nSTATUS: dead");
-alert("You drove it into a ditch");
-console.log("\nDIRECTION LIST: " + d);
-console.log("\nCOMMAND LIST: " + i);
-console.log("\nINVALID COMMAND COUNT: " + inv + "\n");
-}
-// PROCESS FOR DIRECTION
-
-function turnLeft(d,i,a){
-  // alert("ROVER TURNED LEFT");
-  switch(d[a]) {
-    case "n":
-      d.push("w");
-      break;
-    case "s":
-      d.push("e");
-      break;
-    case "w":
-      d.push("s");
-      break;
-    case "e":
-      d.push("n");
-      break;
-    default:
-      break;
-    }
-}
-
-function turnRight(d,i,a){
-    // alert("ROVER TURNED RIGHT");
-    switch(d[a]) {
-      case "n":
-        d.push("e");
-        break;
-      case "s":
-        d.push("w");
-        break;
-      case "w":
-        d.push("n");
-        break;
-      case "e":
-        d.push("s");
-        break;
-      default:
-        break;
-    }
-}
-
-// PROCESS FOR POSITION
-
-function moveFY(d,y) {
-  if (d[d.length-1] == "n") return(y+1);
-    else if (d[d.length-1] == "s") return(y-1);
-}
-
-function moveFX(d,x) {
-  if (d[d.length-1] == "e") return(x+1);
-    else if (d[d.length-1] == "w") return(x-1);
-}
-
-function moveBY(d,y) {
-  if (d[d.length-1] === "n") return(y-1);
-    else if (d[d.length-1] === "s")return(y+1);
-}
-
-function moveBX(d,x) {
-  if (d[d.length-1] === "e") return(x-1);
-    else if (d[d.length-1] === "w") return(x+1);
-}
-
-// hopefully Adam is integrated
-// var Adam1 = new Adam("Being", 100, 20, 100, [x,y], d);
-// console.log(Adam1);
-
-/// down is viking old code
-// Viking
-// function Viking(name, health, strength){
-//   Soldier.call(this, health, strength);
-//   this.name = name;
-//
-//   this.receiveDamage = function(theDamage){
-//     this.health -= theDamage;
-//     if (this.health > 0){
-//       return this.name + " has received " + theDamage + " points of damage";
-//     } else {
-//       return this.name + " has died in act of combat";
-//     }
-//   };
-//
-//   this.battleCry = function(){
-//     return "Odin Owns You All!";
-//   };
+// console.log("\nADAM'S TRAVEL LOG: ");
+// for(a=0; a < i.length; a++) {
+//   switch(i[a]){
+//     case "f":
+//         { if (d[d.length-1] === "n" || d[d.length-1] === "s")
+//           { y = moveFY(d,y);
+//             d.push(d[d.length-1]);
+//             }
+//           else if (d[d.length-1] === "w" || d[d.length-1] === "e")
+//           { x = moveFX(d,x);
+//             d.push(d[d.length-1]);
+//             }
+//         }
+//         break;
+//     case "b":
+//         { if (d[d.length-1] === "n" || d[d.length-1] === "s")
+//           { y = moveBY(d,y);
+//             d.push(d[d.length-1]);
+//             }
+//           else if (d[d.length-1] === "w" || d[d.length-1] === "e")
+//           { x = moveBX(d,x);
+//             d.push(d[d.length-1]);
+//             }
+//         }
+//         break;
+//     case "r":
+//         turnRight(d,i,a);
+//         break;
+//     case "l":
+//         turnLeft(d,i,a);
+//         break;
+//     default:
+//         inv += 1;
+//         d.push(d[d.length-1]);
+//         // alert("invalid command: " + i[a]);
+//         break;
+//   }
+// // OUTPUT RESULTS
+//     if (x < 0 || y < 0 || x > 10 || y > 10) {t=1; /*alert("YOU DROVE IT IN A DITCH!")*/}
+//     console.log("X:",x + " Y:",y);
 // }
 //
-// Viking.prototype = Object.create(Soldier.prototype);
-//
-// // Saxon
-// function Saxon(health, strength){
-//   Soldier.call(this, health, strength);
-//
-//   this.receiveDamage = function(theDamage){
-//     this.health -= theDamage;
-//     if (this.health > 0){
-//       return "A Saxon has received " + theDamage + " points of damage";
-//     } else {
-//       return "A Saxon has died in combat";
-//     }
-//   };
+// console.log("\nADAM IS CURRENTLY FACING:", compass[d[d.length-1]]);
+// if (t === 1){
+// console.log("\nSTATUS: dead");
+// alert("You drove it into a ditch");
+// console.log("\nDIRECTION LIST: " + d);
+// console.log("\nCOMMAND LIST: " + i);
+// console.log("\nINVALID COMMAND COUNT: " + inv + "\n");
 // }
-// Saxon.prototype = Object.create(Soldier.prototype);
+// // PROCESS FOR DIRECTION
 //
-// // War
-// function War(){
-//   this.vikingArmy = [];
-//   this.saxonArmy = [];
+// function turnLeft(d,i,a){
+//   // alert("ROVER TURNED LEFT");
+//   switch(d[a]) {
+//     case "n":
+//       d.push("w");
+//       break;
+//     case "s":
+//       d.push("e");
+//       break;
+//     case "w":
+//       d.push("s");
+//       break;
+//     case "e":
+//       d.push("n");
+//       break;
+//     default:
+//       break;
+//     }
+// }
 //
-//   War.prototype.addViking=function(aViking){
-//     this.vikingArmy.push(aViking);
-//   };
+// function turnRight(d,i,a){
+//     // alert("ROVER TURNED RIGHT");
+//     switch(d[a]) {
+//       case "n":
+//         d.push("e");
+//         break;
+//       case "s":
+//         d.push("w");
+//         break;
+//       case "w":
+//         d.push("n");
+//         break;
+//       case "e":
+//         d.push("s");
+//         break;
+//       default:
+//         break;
+//     }
+// }
 //
-//   War.prototype.addSaxon=function(aSaxon){
-//     this.saxonArmy.push(aSaxon);
-//   };
+// // PROCESS FOR POSITION
 //
-//   War.prototype.vikingAttack=function(){
-//     var aRandomViking = Math.floor(Math.random() * this.vikingArmy.length);
-//     var aRandomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
-//     var combatResult = this.saxonArmy[aRandomSaxon].receiveDamage(this.vikingArmy[aRandomViking].strength);
+// function moveFY(d,y) {
+//   if (d[d.length-1] == "n") return(y+1);
+//     else if (d[d.length-1] == "s") return(y-1);
+// }
 //
-//     //get rid of the unworthy Saxons
-//     var that = this;
+// function moveFX(d,x) {
+//   if (d[d.length-1] == "e") return(x+1);
+//     else if (d[d.length-1] == "w") return(x-1);
+// }
 //
-//     this.saxonArmy.forEach(function(eachSaxon){
+// function moveBY(d,y) {
+//   if (d[d.length-1] === "n") return(y-1);
+//     else if (d[d.length-1] === "s")return(y+1);
+// }
 //
-//       var index = that.saxonArmy.indexOf(eachSaxon);
-//       if(eachSaxon.health <= 0){
-//         that.saxonArmy.splice(index, 1);
-//         }
-//
-//       });
-//       return combatResult;
-//     };
-//   }
-//
-// War.prototype.saxonAttack=function(){
-//     var aRandomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
-//     var aRandomViking = Math.floor(Math.random() * this.vikingArmy.length);
-//     var combatResult = this.vikingArmy[aRandomViking].receiveDamage(this.saxonArmy[aRandomSaxon].strength);
-//
-//     //get rid of the unworthy Vikings
-//     var that = this;
-//
-//     this.vikingArmy.forEach(function(eachViking){
-//
-//       var index = that.vikingArmy.indexOf(eachViking);
-//       if(eachViking.health <= 0){
-//         that.vikingArmy.splice(index, 1);
-//         }
-//       });
-//       return combatResult;
-//   };
-//
-// War.prototype.showStatus=function(){
-//   if (this.saxonArmy.length === 0){
-//     return "Vikings have won the war of the century!";
-//   } else if(this.vikingArmy.length === 0) {
-//     return "Saxons have fought for their lives and survive another day...";
-//   }
-//     else if(this.saxonArmy.length >= 1 || this.vikingArmy.length >= 1){
-//     return "Vikings and Saxons are still in the thick of battle.";
-//   } else{
-//     return "So that did not quite work...";
-//   }
-// };
+// function moveBX(d,x) {
+//   if (d[d.length-1] === "e") return(x-1);
+//     else if (d[d.length-1] === "w") return(x+1);
+// }
--- source.js
+++ source.js
@@ -12,6 +12,7 @@ var map =[
 [0,0,0,0,0,0,0,0,2,2],
 [0,0,0,0,0,0,0,2,2,2],
 ];
+var header = document.getElementById(header);
 
 var grid = clickableGrid(10, 10, function(el, row, col, i) {
  console.log("You clicked on element:", el);
@@ -25,28 +26,54 @@ var grid = clickableGrid(10, 10, function(el, row, col, i) {
  lastClicked = el;
 });
 
+/*<div class="dropdown">
+<button onclick="myFunction()" class="dropbtn">Dropdown</button>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#contact">Contact</a>
+  </div>
+</div>
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+*/
+
 function clickableGrid(rows, cols, callback) {
   var i = 0;
   var grid = document.createElement('table');
   grid.className = 'grid';
-  for (var r = 0; r < rows; ++r) {
+  for (var r = 0; r < rows; r++) {
     var tr = grid.appendChild(document.createElement('tr'));
     for (var c = 0; c < cols; ++c) {
       var cell = tr.appendChild(document.createElement('td'));
       cell.classList.add("tiles");
-
-      switch(map[r][c]) {
+      cell.classList.add(map.indexOf(map[r]) + "," + map.indexOf(map[c]));
+      switch (map[r][c]) {
         case 1:
-        {cell.appendChild(stoneTileMaker());}
-        break;
+          {
+            cell.appendChild(stoneTileMaker());
+          }
+          break;
         case 2:
-        {cell.appendChild(geyserTileMaker());}
-        break;
+          {
+            cell.appendChild(geyserTileMaker());
+          }
+          break;
         case 3:
-        {cell.appendChild(treeTileMaker());}
-        break;
-        }
-      cell.addEventListener('click', (function(el, r, c, i) {
+          {
+            cell.appendChild(treeTileMaker());
+          }
+          break;
+      }
+      //changing value inside the string can allow us to add click functionality.
+      cell.addEventListener('click',(function(el, r, c, i){
         return function() {
           callback(el, r, c, i);
         };
@@ -56,7 +83,8 @@ function clickableGrid(rows, cols, callback) {
   return grid;
 }
 
-document.body.appendChild(grid);
+document.body.prepend(grid);
+
 
 // background image on canvas
 
@@ -75,10 +103,10 @@ return (x);
 
 function geyserTileMaker() {
 var x = document.createElement("IMG");
-x.setAttribute("src", "geyser.gif");
+x.setAttribute("src", "orc.gif");
 x.setAttribute("class", "geyser");
-x.setAttribute("width", "75px");
-x.setAttribute("height", "75px");
+x.setAttribute("width", "45px");
+x.setAttribute("height", "45px");
 x.setAttribute("alt", "geyser tile");
 var geysTiles = document.getElementsByClassName('geyser');
 return (x);
@@ -97,10 +125,10 @@ return (x);
 
 function stoneTileMaker() {
 var x = document.createElement("IMG");
-x.setAttribute("src", "stone.gif");
+x.setAttribute("src", "Hero.gif");
 x.setAttribute("class", "stone");
-x.setAttribute("width", "75px");
-x.setAttribute("height", "75px");
+x.setAttribute("width", "45px");
+x.setAttribute("height", "45px");
 x.setAttribute("alt", "stone tile");
 var stoneTiles = document.getElementsByClassName('stone');
 return (x);
